2e063aa721f64be2caf07bd6c30faf693373fead
MikaelSantos1 <mikaelsantos120@outlook.com>
2022-07-05 22:20:54 -0300

    scheduling component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,13 @@
 
 import React from 'react';
-import { CarDetails } from './src/screens/CarDetails';
+
 import { useFonts, Archivo_400Regular, Archivo_500Medium, Archivo_600SemiBold } from '@expo-google-fonts/archivo'
 
 import { Inter_400Regular, Inter_500Medium, } from '@expo-google-fonts/inter'
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from 'styled-components/native';
 import theme from './src/styles/theme';
+import { Scheduling } from './src/screens/Scheduling';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,7 +22,7 @@ export default function App() {
   }
   return (
     <ThemeProvider theme={theme}>
-      <CarDetails />
+      <Scheduling />
     </ThemeProvider>
     
   );
---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { useTheme } from 'styled-components';
+import { BackButton } from '../../components/BackButton';
+import ArrowSvg from '../../assets/arrow.svg'
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+import { StatusBar } from 'react-native';
+import { Button } from '../../components/Button';
+
+export function Scheduling(){
+    const theme = useTheme()
+  return (
+    <Container>
+        <Header>
+            <StatusBar
+            barStyle='light-content'
+            translucent
+            backgroundColor='transparent'
+            />
+        <BackButton
+          onPress={() => { }}
+          color={theme.colors.shape}
+        />
+        <Title>
+            Escolha uma {'\n'}
+            data de inicio  {'\n'}
+            do aluguel {'\n'}
+        </Title>
+        <RentalPeriod>
+            <DateInfo>
+                <DateTitle>De</DateTitle>
+                <DateValue selected={false}></DateValue>
+            </DateInfo>
+        
+            <ArrowSvg/>
+            <DateInfo>
+                <DateTitle>Ate</DateTitle>
+                <DateValue selected={true}>10-52</DateValue>
+            </DateInfo>
+            </RentalPeriod>
+      </Header>
+
+      <Content>
+
+      </Content>
+      <Footer>
+        <Button title='Confirmar'/>
+      </Footer>
+    </Container>
+  );
+}
